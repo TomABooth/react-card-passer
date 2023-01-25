cf976f66364610f3a29224240de38aec1c16645b
Tom Booth <tombeezle@gmail.com>
2023-01-25 12:43:02 -0800

    cards now rendering, functionality returned to app

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { GameContext } from '../GameContext.js';
 import Card from './Card';
 
-export default function CardList({ cards, setSelectedCard, player, setFrom, selectedCard }) {
+export default function CardList({ cards, player }) {
+  const { selectedCard, setSelectedCard, setFrom } = useContext(GameContext);
   return (
     <div className="card-list">
       {cards.map((card) => (
